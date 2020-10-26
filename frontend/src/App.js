174a020c290c718fc08d7aa49ad8b5dda46377a8
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-10-26 20:41:00 +0100

    react-bootstrap setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div>
-     <h1>Testing multiple git</h1>
-    </div>
-  );
+    <React.Fragment>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>shoplify</h1>
+        </Container>
+      </main>
+      <Footer />
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
