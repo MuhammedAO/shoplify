78997f0d37629079b68554f65ad7548a7060191a
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-11-03 22:13:19 +0100

    Custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import colors from 'colors'
 import dotenv from 'dotenv'
-import products from './data/products.js'
 import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+
 
 dotenv.config()
 
@@ -11,15 +13,12 @@ connectDB()
 
 const app = express()
 
+app.use('/api/products', productRoutes )
 
-app.get('/api/products', (req, res) => res.json(products))
+app.use(notFound)
+//custom error mdw
+app.use(errorHandler)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-
-  res.json(product)
-})
-
- const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
