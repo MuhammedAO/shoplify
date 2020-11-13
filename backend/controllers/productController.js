05ac38386be6928cf4c9983e3e341f712d721632
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-11-13 22:45:36 +0100

    User auth endpoint

---
+++ backend/controllers/productController.js
@@ -0,0 +1,23 @@
+import Product from '../models/productModel.js'
+import asyncHandler from 'express-async-handler'
+
+// aEH is a simple middleware for handling exceptions inside of async express routes and passing them to your express error handlers.
+
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({})
+  res.json(products)
+})
+
+
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export { getProducts, getProductById }
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import colors from 'colors'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 
@@ -13,7 +14,11 @@ connectDB()
 
 const app = express()
 
-app.use('/api/products', productRoutes )
+app.use(express.json())
+
+app.use('/api/products', productRoutes)
+
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 //custom error mdw
