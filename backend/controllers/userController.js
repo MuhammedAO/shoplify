6f4a851b88356cbf104118f1c72e7c1c09132cce
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-11-14 13:10:24 +0100

    generate jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
-
+import generateToken from '../utils/generateToken.js'
 
 
 // Authenticate user and generate token
@@ -16,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id)
     })
   } else {
     res.status(401)
