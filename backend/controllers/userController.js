69ca736dce916a84f7ac12f4bc5b4ab7a00a5029
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-11-14 22:56:11 +0100

    register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,38 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
+//Register a user
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error(' user already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new Error('invalid user data')
+  }
+})
+
+
 //Get user profile
 const getUserProfile = asyncHandler(async (req, res) => {
 
@@ -40,4 +72,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('user not found')
   }
 })
-export { authUser, getUserProfile }
+export { authUser, registerUser,getUserProfile }
