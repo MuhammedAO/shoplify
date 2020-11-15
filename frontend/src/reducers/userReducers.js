c9b96b4fd1ff4add8d1b8b3f9c3e5e767ec78f21
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-11-15 19:06:19 +0100

    user login reducer & action

---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,24 @@
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from "../constants/userConstants"
+
+
+const initialState = {}
+
+export const userLoginReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+
+    case USER_LOGIN_REQUEST:
+      return { loading: true }
+
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: payload }
+
+    case USER_LOGIN_FAIL:
+      return { loading: false, error: payload }
+
+    case USER_LOGOUT:
+      return {}
+
+    default:
+      return state
+  }
+}
