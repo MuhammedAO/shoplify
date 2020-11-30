e3580140eaf455ad3a4763c267441d621c237e71
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-11-30 19:45:36 +0100

    save payment method

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,9 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants'
+import { 
+  CART_ADD_ITEM, 
+  CART_REMOVE_ITEM, 
+  CART_SAVE_PAYMENT_METHOD, 
+  CART_SAVE_SHIPPING_ADDRESS } 
+  from '../constants/cartConstants'
 
 const initialState = {
   cartItems: [],
@@ -33,6 +38,12 @@ export const cartReducer = (state = initialState, { type, payload }) => {
       return {
         ...state,
         shippingAddress: payload
+      }
+
+    case CART_SAVE_PAYMENT_METHOD:
+      return {
+        ...state,
+        paymentMethod: payload
       }
 
     default:
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
-import {saveShippingAddress} from '../actions/cartActions'
+import { saveShippingAddress } from '../actions/cartActions'
+import CheckoutSteps from '../components/CheckoutSteps'
 
 const ShippingScreen = ({ history }) => {
   const cart = useSelector(state => state.cart)
-  const {shippingAddress} = cart
+  const { shippingAddress } = cart
 
   const [address, setAddress] = useState(shippingAddress.address)
   const [city, setCity] = useState(shippingAddress.city)
@@ -17,12 +18,13 @@ const ShippingScreen = ({ history }) => {
 
   const submitHandler = (e) => {
     e.preventDefault()
-    dispatch(saveShippingAddress({address, city, postalCode, country}))
+    dispatch(saveShippingAddress({ address, city, postalCode, country }))
     history.push('/payment')
   }
- 
+
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>
