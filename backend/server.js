e650c1c61ec4f662fdebb95343b10aab4dc38ebd
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-11-30 23:57:54 +0100

    order route & controllers

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 
@@ -19,6 +20,8 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 
 app.use('/api/users', userRoutes)
+
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 //custom error mdw
